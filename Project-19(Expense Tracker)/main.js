f1a60bf7eb552c2772077a55f3e934a31ad4f2b7
Sindhuja <kalagotlasindhuja06@gmail.com>
2026-02-10 22:39:33 +0530

    Add files via upload

---
+++ Project-19(Expense Tracker)/main.js
@@ -0,0 +1,64 @@
+ const balance = document.getElementById("balance");
+    const moneyPlus = document.getElementById("money-plus");
+    const moneyMinus = document.getElementById("money-minus");
+    const list = document.getElementById("list");
+    const form = document.getElementById("form");
+    const text = document.getElementById("text");
+    const amount = document.getElementById("amount");
+
+    let transactions = [];
+
+    function addTransaction(e) {
+      e.preventDefault();
+
+      const transaction = {
+        id: Date.now(),
+        text: text.value,
+        amount: +amount.value
+      };
+
+      transactions.push(transaction);
+      addTransactionDOM(transaction);
+      updateValues();
+
+      text.value = "";
+      amount.value = "";
+    }
+
+    function addTransactionDOM(transaction) {
+      const sign = transaction.amount < 0 ? "-" : "+";
+      const item = document.createElement("li");
+
+      item.classList.add(transaction.amount < 0 ? "minus" : "plus");
+
+      item.innerHTML = `
+        ${transaction.text}
+        <span>${sign}₹${Math.abs(transaction.amount)}</span>
+        <button class="delete-btn" onclick="removeTransaction(${transaction.id})">×</button>
+      `;
+
+      list.appendChild(item);
+    }
+
+    function updateValues() {
+      const amounts = transactions.map(t => t.amount);
+
+      const total = amounts.reduce((acc, item) => acc + item, 0).toFixed(2);
+      const income = amounts.filter(a => a > 0).reduce((acc, a) => acc + a, 0).toFixed(2);
+      const expense = (
+        amounts.filter(a => a < 0).reduce((acc, a) => acc + a, 0) * -1
+      ).toFixed(2);
+
+      balance.innerText = `₹${total}`;
+      moneyPlus.innerText = `₹${income}`;
+      moneyMinus.innerText = `₹${expense}`;
+    }
+
+    function removeTransaction(id) {
+      transactions = transactions.filter(t => t.id !== id);
+      list.innerHTML = "";
+      transactions.forEach(addTransactionDOM);
+      updateValues();
+    }
+
+    form.addEventListener("submit", addTransaction);
